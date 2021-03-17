79332e449667d6c0cf2eb380b4ae6b3f72d03955
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2021-03-17 09:55:09 +0100

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -10,14 +10,7 @@ import "normalize.css/normalize.css"
 import "react-dates/lib/css/_datepicker.css"
 import "./styles/style.scss"
 
-const expenseOne = store.dispatch(addExpense({description: "Rent", note: "House rent for February", amount: 5000, createdAt: 1250}))
-const expenseTwo = store.dispatch(addExpense({ description: "Coffee", note: "Early morning Coffee", amount: 500, createdAt: 1400 }))
-const expenseThree = store.dispatch(addExpense({description: "Fruits", note: "I need to eat Banana fruit tonight", amount: 10000, createdAt: -10}))
-const expenseFour = store.dispatch(addExpense({description: "Gas Bill", note: "Gas bill for the month of February", amount: 900, createdAt: 3020}))
-const expenseFive = store.dispatch(addExpense({description: "Water Bill", note: "Water bill for the month of February", amount: 2030, createdAt: -1000}))
 
-
- 
 //With this I have been able to provide the store for all my component to mae use of it.
 const jsx = (
  <Provider store={store}>
