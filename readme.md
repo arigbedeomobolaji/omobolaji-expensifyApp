d19a15958d18666b866d6a7e7bce6c67cc403c65
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2021-03-19 15:41:28 +0100

    update the code for the third time

---
+++ readme.md
@@ -0,0 +1,9 @@
+# readme.md (markdown) file
+
+# Git Commands
+
+git init - it allows us to create a new git repository
+git status - it alllows you to view thr changes to your project code
+git add - it allows us to add files to staging area
+git commit - creates a new commit with files from the staging area
+git log - let you view recent commits
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,52 @@
 const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-module.exports = {
- mode: "development",
- entry: {
-  app: path.join(__dirname, "src/app.js")
- },
- output: {
-  path: path.join(__dirname, "public"),
-  filename: "app.bundle.js"
- },
- module: {
-  rules: [{
-   test: /\.js$/,
-   exclude: /node-modules/,
-   use: "babel-loader"
+module.exports = (env) => {
+ const cssExtractPlugin = new MiniCssExtractPlugin({
+  filename: "style.css"
+ })
+
+ return ({
+  mode: env.production ? "production": "development",
+  entry: {
+   app: path.join(__dirname, "src/app.js")
   },
-  {
-   test: /\.s?css$/,
-   use: [
-    "style-loader",
-    "css-loader",
-    "sass-loader"
-   ]
-  }]
- },
- devtool: "eval-cheap-module-source-map",
- devServer: {
-  contentBase: path.join(__dirname, "public"),
-  compress: true,
-  port: 8080,
-  historyApiFallback: true
- }
+  output: {
+   path: path.join(__dirname, "public"),
+   filename: "app.bundle.js"
+  },
+  module: {
+   rules: [{
+    test: /\.js$/,
+    exclude: /node-modules/,
+    use: "babel-loader"
+   },
+   {
+    test: /\.s?css$/,
+    use: [
+     MiniCssExtractPlugin.loader,{
+      loader: "css-loader",
+      options: {
+       sourceMap: true
+      }
+     }, {
+      loader: "sass-loader",
+      options: {
+       sourceMap: true
+      }
+     }
+    ]
+   }]
+  },
+  plugins: [
+   cssExtractPlugin
+  ],
+  devtool: env.production ? "source-map" : "inline-source-map",
+  devServer: {
+   contentBase: path.join(__dirname, "public"),
+   compress: true,
+   port: 8080,
+   historyApiFallback: true
+  }
+ })
 }
